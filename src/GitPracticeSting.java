2ab135f22f9ab898ed7eccc95fe4d20768bda5d9
Mithunganesh <mithunganesh1@gmail.com>
2022-02-07 00:22:57 +0530

    squashing 2 java commits
    Description for the commit for demo with squash and reword

---
+++ src/GitPracticeSting.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*
+
+public class GitPracticeString{
+	public static void main(Styring ar[]){
+	System.out.println("hello");
+	System.out.println("Welcome to git practice");
+	}
+}
